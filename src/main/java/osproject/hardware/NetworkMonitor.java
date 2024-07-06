a872480e5b5fce0a174dfb482a72f33e4e555c78
oberrya <82906586+oberrya@users.noreply.github.com>
2024-07-06 19:34:24 -0400

    Add files via upload

--- src/main/java/osproject/hardware/NetworkMonitor.java
+++ src/main/java/osproject/hardware/NetworkMonitor.java
@@ -1,15 +1,32 @@
 package osproject.hardware;
 
 import java.util.ArrayList;
-
+import oshi.SystemInfo;
+import oshi.hardware.NetworkIF;
 import osproject.DoubleObserver;
 
 public class NetworkMonitor implements HardwareMonitor {
 
     @Override
     public Double getLoadPercent() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getLoadPercent'");
+        SystemInfo systemInfo = new SystemInfo();
+        NetworkIF[] networkIFs = systemInfo.getHardware().getNetworkIFs();
+
+        if (networkIFs != null) {
+            for (NetworkIF net : networkIFs) {
+                System.out.println("Name: " + net.getName());
+                System.out.println("  MAC Address: " + net.getMacaddr());
+                System.out.println("  IPv4 Address: " + net.getIPv4addr());
+                System.out.println("  IPv6 Address: " + net.getIPv6addr());
+                System.out.println("  Bytes Sent: " + net.getBytesSent());
+                System.out.println("  Bytes Received: " + net.getBytesRecv());
+                System.out.println("  Packets Sent: " + net.getPacketsSent());
+                System.out.println("  Packets Received: " + net.getPacketsRecv());
+                System.out.println();
+            }
+        } else {
+            System.out.println("No network interfaces found.");
+        }
     }
 
     @Override
